821babb6dd234b69b2d32383e5b4f82da4a6364f
hturner7635 <159948963+hturner7635@users.noreply.github.com>
2024-02-18 12:01:50 -0500

    Add files via upload

---
+++ Images/app.js
@@ -0,0 +1,14 @@
+const accordianTitles = document.querySelectorAll(".accordian-title");
+
+accordianTitles.forEach((accordianTitle) => {
+    accordianTitle.addEventListener("click", () => {
+        const height = accordianTitle.nextElementSibling.scrollHeight;
+        console.log(height);  
+        accordianTitle.classList.toggle("active-header");
+        if (accordianTitle.classList.contains("active-header")) {
+            accordianTitle.nextElementSibling.style.maxHeight = `${height}px`;
+        } else {
+            accordianTitle.nextElementSibling.style.maxHeight = "0px";
+        }
+    });
+});
